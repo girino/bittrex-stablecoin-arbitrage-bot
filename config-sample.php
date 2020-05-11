63a7ae27f685fed90cb276268509b73465aa496c
girinovey <girinovey@girino.org>
2020-05-11 16:48:05 -0300

    config sample

---
+++ config-sample.php
@@ -0,0 +1,11 @@
+<?php
+$config = array(
+    "API_KEY" => "API_KEY",
+    "API_SECRET" => "API_SECRET",
+    "BASE_CURRENCY" => "USDT",
+    "MARKET_CURRENCY" => "USDC",
+    "BUY_PRICE" => 0.9986,
+    "SELL_PRICE" =>  1.0198,
+    "CAPITAL" => 50.0
+);
+?>
