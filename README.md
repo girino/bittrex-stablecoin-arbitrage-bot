ad568ee00684fc608e9773c473323c8f4f2f854f
girinovey <girinovey@girino.org>
2020-05-12 01:52:22 -0300

    comand line params, new features

---
+++ README.md
@@ -0,0 +1,31 @@
+# bittrex-stablecoin-arbitrage-bot
+
+This project is a port to php and bittrex of my previous bot that worked on binance and ran on nodejs: https://github.com/girino/TUSD-USDT-Infinity-Profit-Bot
+
+This one has also some imporvements, such as measurement of best markets and best parameter per market.
+
+Rename the ```config-sample.php``` to ```config.php```, fill in your API key and run with ```php main.php```.
+
+PHP was not a choice, but rather an imposition because existing bittrex API client implementations in both python ans nodejs (my first and second choices) were abandoned for more than 2 years. This php client from "codenix-sv" seems to be well maintained.
+
+some fun parameters to try:
+
+```php main.php -c```
+
+Cancels all open orders
+
+```php main.php -s```
+
+Searches for the most profitable parameters for a single market (by default, the one the config file).
+
+```php main.php -b```
+
+Searches for the most profitable parameters for all markets that have the current price between 0.98 and 1.02. (beware, not all of them are stablecoins. make sure you use only with stablecoins)
+
+Both the ```-b``` and ```-s``` accpet the following params:
+
+```--interval```: the time interval for the candles to be analized (can be 'm', '5m', 'h' or 'd', meaning 1 minute, 5 minutes, 1 hour and 1 day respectively).
+```--min-price```: the minimum price to search (defaults to 0.98)
+```--max-price```: the maximum price to search (defaults to 1.02)
+```--step```: the increment on the values searched (defaults to 0.0001)
+```--market```: (only in ```-s```) searches a different market from the one in the config file.
--- main.php
+++ main.php
@@ -21,9 +21,9 @@ class V3Api extends Api {
         return $this->transformer->transform($response);
     }
 
-    public function getCandles($market): array
+    public function getCandles($market, $interval): array
     {
-        return $this->getv3('/markets/' . $market . '/candles/HOUR_1/recent');
+        return $this->getv3('/markets/' . $market . '/candles/' . $interval . '/recent');
         // return $this->getv3('/markets/' . $market . '/candles/HOUR_1/historical/2020/04/01');
         // return $this->getv3('/markets/' . $market . '/candles/HOUR_1/historical/2020/03/01');
         // return $this->getv3('/markets/' . $market . '/candles/HOUR_1/historical/2020/02/01');
@@ -57,7 +57,7 @@ $client->setCredential($config['API_KEY'], $config['API_SECRET']);
 
 function singleMarket($market, $buyPrice, $sellPrice) {
     global $client;
-    $candles = $client->v3Api()->getCandles($market);
+    $candles = $client->v3Api()->getCandles($market, 'HOUR_1');
     return calculate($candles, $buyPrice, $sellPrice);
 }
 
@@ -82,49 +82,145 @@ function calculate($candles, $buyPrice, $sellPrice) {
     return array($val, $count);
 }
 
-function getBestMarkets() {
+function getBestSingleMarket($symbol, $interval, $minPrice, $maxPrice, $step) {
+    global $client;
+    $candles = $client->v3Api()->getCandles($symbol, $interval);
+    $maxVal = 0.0;
+    $maxState = array(0,0,0,0);
+    for ($buyPrice = $maxPrice; $buyPrice > $minPrice; $buyPrice -= $step) {
+        for ($sellPrice = $buyPrice + $step; $sellPrice < $maxPrice; $sellPrice += $step) {
+            $retArray = calculate($candles, $buyPrice, $sellPrice);
+            $val = $retArray[0]; $count = $retArray[1];
+            if ($val > $maxVal) {
+                $maxVal = $val;
+                $maxState = array($buyPrice, $sellPrice, $val, $count, $symbol);
+            }
+        }
+    }
+    return $maxState;
+}
+
+function printMarket($marketArray) {
+    print($marketArray[4] . ":\n");
+    $maxStateVal = "    [" . $marketArray[0] . ', ' . $marketArray[1] . ']: ' . (($marketArray[2]-1)*100) . " (" . $marketArray[3] . ")\n";
+    print ($maxStateVal);
+}
+
+
+function printBestMarkets($interval, $minPrice = 0.98, $maxPrice = 1.02, $step=0.0001) {
 
     global $client;
-    $markets = array();
-
     $data = $client->v3Api()->getmarketsummaries();
     foreach($data as $result) {
         $mid = ($result['high']+$result['low'])/2.0;
-        if ($mid > 0.98 && $mid < 1.02) {
-            // print($result['symbol'] . ": " . $mid . "\n");
-            array_push($markets, $result);
+        if ($mid > $minPrice && $mid < $maxPrice) {
+            printMarket(getBestSingleMarket($result['symbol'], $interval, $minPrice, $maxPrice, $step));
         }
     }
 
-    foreach($markets as $market) {
-        $candles = $client->v3Api()->getCandles($market['symbol']);
-        print($market['symbol'] . ":\n");
-        $maxVal = 0.0;
-        $maxStateVal = 'No profit';
-        for ($buyPrice = 1.02; $buyPrice > 0.98; $buyPrice -= 0.0001) {
-            for ($sellPrice = $buyPrice + 0.0001; $sellPrice < 1.02; $sellPrice += 0.0001) {
-                $retArray = calculate($candles, $buyPrice, $sellPrice);
-                $val = $retArray[0]; $count = $retArray[1];
-                if ($val > $maxVal) {
-                    $maxStateVal = "    [" . $buyPrice . ', ' . $sellPrice . ']: ' . (($val-1)*100) . " (" . $count . ")\n";
-                    $maxVal = $val;
-                }
-            }
-        }
-        print ($maxStateVal);
+}
+
+function cancelAllOrders() {
+    global $client;
+    $orders = $client->market()->getOpenOrders()['result'];
+    // cancel old orders
+    foreach($orders as $order) {
+        printf("Canceling uuid: %s... ", $order['OrderUuid']);
+        $client->market()->cancel($order['OrderUuid']);
+        printf("Done.\n");
     }
 }
 
+function getInterval($options) {
+    global $client;
+    $interval = array_key_exists("interval", $options) ? $options['interval'] : 'h';
+    $interval = strtolower($interval);
+    $values_map = array(
+        'm' => 'MINUTE_1',
+        '5m' => 'MINUTE_5',
+        'h' => 'HOUR_1',
+        'd' => 'DAY_1',
+    );
+    if (!array_key_exists($interval, $values_map)) {
+        printf('ERROR: no interval for %s\n', $interval);
+        exit(1);
+    }
+    return $values_map[$interval];
+}
+function getMinPrice($options) {
+    return array_key_exists("min-price", $options) ? floatval($options['min-price']) : 0.98;
+}
+
+function getMaxPrice($options) {
+    return array_key_exists("max-price", $options) ? floatval($options['max-price']) : 1.02;
+}
+
+function getStep($options) {
+    return array_key_exists("step", $options) ? floatval($options['step']) : 0.0001;
+}
+
+function getMarketName($options) {
+    global $config;
+    return array_key_exists("market", $options) ? $options['market'] : $config['MARKET_CURRENCY'] . '-' . $config['BASE_CURRENCY'];
+}
+
+
 // main
 if (php_sapi_name() == "cli") {
-    $options = getopt("b", ['best']);
+    $options = getopt("abcns", ['auto', 'single', 'best', 'cancel', 'no-cancel', 'min-price:', 'max-price:', 'interval:', 'step:', 'market:']);
     if (array_key_exists("b", $options) || array_key_exists("best", $options)) {
-        getBestMarkets();
+        $interval = getInterval($options);
+        $min = getMinPrice($options);
+        $max = getMaxPrice($options);
+        $step = getStep($options);
+        printf("Searching best markets for interval '%s', and prices from %f to %f with step %f\n", $interval, $min, $max, $step);
+        printBestMarkets($interval, $min, $max, $step);
         exit(0);
     }
-    $marketName = $config['BASE_CURRENCY'] . '-' . $config['MARKET_CURRENCY'];
-    $balanceBaseCurrency = $client->account()->getBalance($config['BASE_CURRENCY'])['result'];
-    $balanceMarketCurrency = $client->account()->getBalance($config['MARKET_CURRENCY'])['result'];
+    if (array_key_exists("s", $options) || array_key_exists("single", $options)) {
+        $interval = getInterval($options);
+        $min = getMinPrice($options);
+        $max = getMaxPrice($options);
+        $step = getStep($options);
+        $market = getMarketName($options);
+        printf("Searching %s for interval '%s', and prices from %f to %f with step %f\n", $market, $interval, $min, $max, $step);
+        printMarket(getBestSingleMarket($market, $interval, $min, $max, $step));
+        exit(0);
+    }
+    if (array_key_exists("c", $options) || array_key_exists("cancel", $options)) {
+        cancelAllOrders();
+        exit(0);
+    }
+
+    // reads all params here
+    $buyPrice = $config['BUY_PRICE'];
+    $sellPrice = $config['SELL_PRICE'];
+    $capital = $config['CAPITAL'];
+    $baseCurrency = $config['BASE_CURRENCY'];
+    $marketCurrency = $config['MARKET_CURRENCY'];
+
+    // if auto
+    if (array_key_exists("a", $options) || array_key_exists("auto", $options)) {
+        $interval = getInterval($options);
+        $min = getMinPrice($options);
+        $max = getMaxPrice($options);
+        $step = getStep($options);
+        $market = getMarketName($options);
+        printf("Searching %s for interval '%s', and prices from %f to %f with step %f\n", $market, $interval, $min, $max, $step);
+        $maxState = getBestSingleMarket($market, $interval, $min, $max, $step);
+        printMarket($maxState);
+        $buyPrice = $maxState[0];
+        $sellPrice = $maxState[1];
+    }
+
+    // cancel by default unless n or no-cancel is specified
+    if (!array_key_exists("n", $options) && !array_key_exists("no-cancel", $options)) {
+        cancelAllOrders();
+    } else {
+        printf("Not cancelling orders.\n");
+    }
+
+    $marketName = $baseCurrency . '-' . $marketCurrency;
     // market infor
     $markets = $client->public()->getMarkets()['result'];
     $marketParams = array();
@@ -134,65 +230,52 @@ if (php_sapi_name() == "cli") {
         break;
         }
     }
-    print_r($marketParams);
     while (true) {
         // system('cls');
         try {
-            printf("============ Starting new Cycle ==========\n");
             $marketSummary = $client->public()->getMarketSummary($marketName)['result'];
-            $balanceBaseCurrency = $client->account()->getBalance($config['BASE_CURRENCY'])['result'];
-            $balanceMarketCurrency = $client->account()->getBalance($config['MARKET_CURRENCY'])['result'];
+            $balanceBaseCurrency = $client->account()->getBalance($baseCurrency)['result'];
+            $balanceMarketCurrency = $client->account()->getBalance($marketCurrency)['result'];
             $total = $balanceMarketCurrency['Balance'] + $balanceBaseCurrency['Balance'];
-            $lucro = $balanceMarketCurrency['Balance'] + $balanceBaseCurrency['Balance'] - $config['CAPITAL'];
+            $lucro = $balanceMarketCurrency['Balance'] + $balanceBaseCurrency['Balance'] - $capital;
             $orders = $client->market()->getOpenOrders()['result'];
-            $orderHistory = $client->account()->getOrderHistory($marketName)['result'];
             system('clear');
             printf("==========================================\n");
-            printf("SALDO %4s...: %14.8f (%14.8f + %14.8f)\n", $config['MARKET_CURRENCY'], $balanceMarketCurrency['Balance'], $balanceMarketCurrency['Available'], $balanceMarketCurrency['Pending']);
-            printf("SALDO %4s...: %14.8f (%14.8f + %14.8f)\n", $config['BASE_CURRENCY'], $balanceBaseCurrency['Balance'], $balanceBaseCurrency['Available'], $balanceBaseCurrency['Pending']);
-            printf("SALDO TOTAL..: %14.8f %s\n", $total, $config['BASE_CURRENCY']);
-            printf("SALDO INICIAL: %14.8f %s\n", $config['CAPITAL'], $config['BASE_CURRENCY']);
-            printf("LUCRO........: %14.8f %s\n", $lucro, $config['BASE_CURRENCY']);
+            printf("SALDO %4s...: %14.8f (%14.8f + %14.8f)\n", $marketCurrency, $balanceMarketCurrency['Balance'], $balanceMarketCurrency['Available'], $balanceMarketCurrency['Pending']);
+            printf("SALDO %4s...: %14.8f (%14.8f + %14.8f)\n", $baseCurrency, $balanceBaseCurrency['Balance'], $balanceBaseCurrency['Available'], $balanceBaseCurrency['Pending']);
+            printf("SALDO TOTAL..: %14.8f %s\n", $total, $baseCurrency);
+            printf("SALDO INICIAL: %14.8f %s\n", $capital, $baseCurrency);
+            printf("LUCRO........: %14.8f %s\n", $lucro, $baseCurrency);
+            printf("%s\n", date('Y-m-d H:i:s T', time()));
             printf("==========================================\n");
             printf("Cotação     %s %10.8f\n", $marketName, $marketSummary[0]['Last']);
             printf("Preço Medio %s %10.8f\n", $marketName, $marketSummary[0]['BaseVolume']/$marketSummary[0]['Volume']);
-            printf("Definidos: compra %10.8f e venda %10.8f\n", $config['BUY_PRICE'], $config['SELL_PRICE']);
+            printf("Definidos: compra %10.8f e venda %10.8f\n", $buyPrice, $sellPrice);
             printf("============== Open Orders ===============\n");
-            // cancel unmatching orders
             foreach($orders as $order) {
                 $type = ($order['OrderType'] == 'LIMIT_SELL') ? 'SELL' : ' BUY';
                 $price = $order['PricePerUnit'] > 0 ? $order['PricePerUnit'] : $order['Limit'];
                 printf("%s %s %14.8f (was %14.8f) @ %14.8f\n", substr($order['Opened'], 0, 10), $type, $order['QuantityRemaining'], $order['Quantity'], $price);
-                // print_r($order);
-                if ($order['Exchange'] != $marketName) {
-                    print_r($client->market()->cancel($order['uuid']));
-                }
             }
             printf("============== Order History =============\n");
-            // cancel unmatching orders
+            $orderHistory = $client->account()->getOrderHistory($marketName)['result'];
             foreach($orderHistory as $order) {
                 $type = ($order['OrderType'] == 'LIMIT_SELL') ? 'SELL' : ' BUY';
                 $price = $order['PricePerUnit'] > 0 ? $order['PricePerUnit'] : $order['Limit'];
                 printf("%s %s %14.8f (was %14.8f) @ %14.8f\n", substr($order['Closed'], 0, 10), $type, $order['QuantityRemaining'], $order['Quantity'], $price);
                 // print_r($order);
             }
-            printf("==========================================\n");
+            // printf("==========================================\n");
 
-            // cancel old orders
-            foreach($orders as $order) {
-                if ($order['Exchange'] != $marketName) {
-                    print_r($client->market()->cancel($order['uuid']));
-                }
-            }
             // open orders with new balance
-            $amount = round($balanceBaseCurrency['Available']/$config['BUY_PRICE']*(1.0-0.002), 8, PHP_ROUND_HALF_DOWN);
+            $amount = round($balanceBaseCurrency['Available']/$buyPrice*(1.0-0.002), 8, PHP_ROUND_HALF_DOWN);
             if ($amount > $marketParams['MinTradeSize']) {
-                printf("BUYING %.8f at %.8f\n",$amount, $config['BUY_PRICE']);
-                print_r($client->market()->buyLimit($marketName, $amount, $config['BUY_PRICE']));
+                printf("BUYING %.8f at %.8f\n",$amount, $buyPrice);
+                print_r($client->market()->buyLimit($marketName, $amount, $buyPrice));
             }
             if ($balanceMarketCurrency['Available'] > $marketParams['MinTradeSize']) {
-                printf("SELLING %.8f at %.8f\n", $balanceMarketCurrency['Available'], $config['SELL_PRICE']);
-                print_r($client->market()->sellLimit($marketName, $balanceMarketCurrency['Available'], $config['SELL_PRICE']));
+                printf("SELLING %.8f at %.8f\n", $balanceMarketCurrency['Available'], $sellPrice);
+                print_r($client->market()->sellLimit($marketName, $balanceMarketCurrency['Available'], $sellPrice));
             }
 
             sleep(10);
